4529789c410d666be4f4e4acfeaaad658c472385
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-18 12:58:25 -0800

    set two default enemies and added displayEnemies function

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/* Imports */
-
-/* Get DOM Elements */
 const enemiesEl = document.getElementById('enemies');
 const starterHPEl = document.getElementById('starter-hp');
 const starterImgEl = document.getElementById('starter-img');
@@ -8,12 +5,56 @@ const inputEl = document.getElementById('enemy-input');
 const buttonEl = document.getElementById('enemy-button');
 const winsCountEl = document.getElementById('wins-count');
 
-/* State */
 let winsCount = 0;
 let starterHP = 10;
+const enemies = [
+    {
+        name: 'Steve',
+        hp: 5,
+    },
+    {
+        name: 'Susan',
+        hp: 2,
+    },
+];
 
-/* Events */
+function displayEnemies() {
+    enemiesEl.textContent = '';
 
-/* Display Functions */
+    for (let enemy of enemies) {
+        const newEnemyEl = renderEnemy(enemy);
 
-// (don't forget to call any display functions you want to run on page load!)
+        newEnemyEl.addEventListener('click', () => {
+            if (Math.random() > 0.3) {
+                alert('charmander hit ' + enemy.name);
+                enemy.hp--;
+
+                if (enemy.hp === 0) {
+                    winsCount++;
+                    winsCount.textContent = `Pokémon defeated: ${winsCount}`;
+                }
+            } else {
+                alert('the attack missed ' + enemy.name);
+            }
+
+            if (Math.random() > 0.8) {
+                alert(enemy.name + 'used lick');
+                starterHP--;
+
+                if (playerHP <= 0) {
+                    starterImgEl.classList.add('fainted-starter');
+                }
+            } else {
+                alert(enemy.name + 'used lick, but it missed');
+            }
+
+            starterHPEl.textContent = starterHP;
+
+            displayEnemies();
+        });
+
+        enemiesEl.append(newEnemyEl);
+    }
+}
+
+displayEnemies();
