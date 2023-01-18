b79357b7400568c1fe4c30214816d22cfbc32ed6
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-18 11:45:33 -0800

    grabbed DOM elements and declared state

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 /* Imports */
 
 /* Get DOM Elements */
+const enemiesEl = document.getElementById('enemies');
+const starterHPEl = document.getElementById('starter-hp');
+const starterImgEl = document.getElementById('starter-img');
+const inputEl = document.getElementById('enemy-input');
+const buttonEl = document.getElementById('enemy-button');
+const winsCountEl = document.getElementById('wins-count');
 
 /* State */
+let winsCount = 0;
+let starterHP = 10;
 
 /* Events */
 
